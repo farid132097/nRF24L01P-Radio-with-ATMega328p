646eee24695e68d4d87641f743c40802e28cfb51
farid132097 <82755186+farid132097@users.noreply.github.com>
2021-04-18 16:16:21 +0600

    Update main.c

--- main.c
+++ main.c
@@ -12,10 +12,7 @@ RF_START(2); // set channel number
 
 while(1){
            
-           RF_WAKE_UP(); //optional if you don't care power consumption
-           RF_TX_TEXT("nRF24L01+ UD");
-	   RF_SLEEP();   //optional if you don't care power consumption
-		   
+           RF_TX_TEXT("nRF24L01+ Says Hello!");   
            _delay_ms(500);
 		   
         }
