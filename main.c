452c3dd2789af7dc5db6b5a1bd2cdc0afde0223b
farid132097 <82755186+farid132097@users.noreply.github.com>
2021-04-18 16:20:20 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 
-//TX 0x11
+//Default TX address 0x11
+//Change if necessary in nRF24L01P.h definitions
+//Transmitter sample code
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -12,8 +14,22 @@ RF_START(2); // set channel number
 
 while(1){
            
+	  //For Transmitter Node
            RF_TX_TEXT("nRF24L01+ Says Hello!");   
            _delay_ms(500);
+	
+	/*
+	  //for Receiver Node uncomment this section
+	  
+	  uint8_t rx_buf[32],rx_data_len=0;
+		   
+          if(RF_RX(rx_buf,&rx_data_len)){
+	      //received data in 'rx_buf' buffer
+              //do your stuff after receiving
+	     }
+	  _delay_ms(1);
+        */
 		   
         }
 }
+
