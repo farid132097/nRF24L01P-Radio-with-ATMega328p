a4ae48c420fc54006989cbfbd6a7cf98bf17a7b7
farid132097 <82755186+farid132097@users.noreply.github.com>
2021-04-19 06:50:44 +0600

    Update nRF24L01P.h

--- nRF24L01P.h
+++ nRF24L01P.h
@@ -9,21 +9,36 @@ NRF MOSI -> ATmega328P PB3 (Hardware SPI pin, can't be changed)
 NRF CSN  -> ATmega328P PD6 (Configureable, see below definitions)
 NRF CE   -> ATmega328P PD5 (Configureable, see below definitions)
 
-
-RF FUNCTIONS USED:
-
-void    RF_START(set_rf_channel)  
+--------------------------------------------------------------------------------------------------------
+|    RF FUNCTIONS USED:                                                                                |
+--------------------------------------------------------------------------------------------------------
+void    RF_START(set_rf_channel) 
 void    RF_SLEEP(void)
 void    RF_WAKE_UP(void)
 void    RF_TX_TEXT(char_buffer)
 uint8_t RF_TX(tx_buf, set_tx_data_len, set_rx_addr)
 uint8_t RF_RX(rx_buf, get_rx_data_len)
-uint8_t RF_RX_TIMEOUT(rx_buf, get_rx_data_len, rx_timeout, rx_buf_flash)
+uint8_t RF_RX_TIMEOUT(rx_buf, get_rx_data_len, rx_timeout, rx_buf_flush)
 uint8_t RF_TX_ACK(tx_buf, set_tx_data_len, set_rx_addr, tx_retry)
 uint8_t RF_RX_ACK(rx_buf, get_rx_data_len)
 uint8_t RF_TX_GET_ACK_PACKET(tx_buf, set_tx_data_len, rx_buf, get_rx_data_len, set_rx_addr, tx_retry)
 uint8_t RF_RX_GET_ACK_PACKET(rx_buf, get_rx_data_len, tx_buf, set_tx_data_len)
 
+-------------------------------------------------------------------------------------------------------
+|    Function  Prototypes Used:                                                                       |
+-------------------------------------------------------------------------------------------------------
+|  type   |       name         |         comment                                                      |
+-------------------------------------------------------------------------------------------------------
+uint8_t      set_rf_channel       single byte (max value 127)
+char         char_buffer          character type array
+uint8_t      tx_buf               uint8_t type array (must be 32 bytes long)
+uint8_t      rx_buf               uint8_t type array (must be 32 bytes long)
+uint8_t      set_tx_data_len      single byte (max value 26)
+*uint8_t     get_rx_data_len      single byte (use &variable)
+uint8_t      set_rx_addr          device address which will receive data; single byte (max value 255)
+uint16_t     rx_timeout           set timeout in ms (max value 6500)
+uint16_t     rx_buf_flush         boolean input (to flush every time before checking new data set it 1)
+uint8_t      tx_retry             max retries when no acknowledgement received (max value 255)
 */
 
 
@@ -34,51 +49,48 @@ uint8_t RF_RX_GET_ACK_PACKET(rx_buf, get_rx_data_len, tx_buf, set_tx_data_len)
 #include <stdio.h>
 
 
-//Configure this section//
-#define  CSN_DDR           DDRD   /*Change CSN Data direction if necessary*/
-#define  CSN_PORT          PORTD  /*Change CSN port if necessary*/
-#define  CSN               6      /*Change CSN pin number if necessary*/
 
-#define  CE_DDR            DDRD   /*Change CE Data direction if necessary*/
-#define  CE_PORT           PORTD  /*Change CE port if necessary*/
-#define  CE                5      /*Change CE pin number if necessary*/
+#define  CSN_DDR           DDRD   /* Change CSN Data direction if necessary */
+#define  CSN_PORT          PORTD  /* Change CSN port if necessary */
+#define  CSN               6      /* Change CSN pin number if necessary */
 
-#define  OWN_ADDR          0x11   /*Change Node Address*/
-//Configure this section//
+#define  CE_DDR            DDRD   /* Change CE Data direction if necessary */
+#define  CE_PORT           PORTD  /* Change CE port if necessary */
+#define  CE                5      /* Change CE pin number if necessary */
 
+#define  OWN_ADDR          0x11   /* Change Node Address */
 
 
 
-//DO NOT CHANGE THIS SECTION//
-#define  SCK_DDR           DDRB   
-#define  SCK_PORT          PORTB  
-#define  SCK               5      
+#define  SCK_DDR           DDRB   /* Do not change */
+#define  SCK_PORT          PORTB  /* Do not change */
+#define  SCK               5      /* Do not change */
 
-#define  MISO_DDR          DDRB   
-#define  MISO_PORT         PORTB  
-#define  MISO              4      
+#define  MISO_DDR          DDRB   /* Do not change */
+#define  MISO_PORT         PORTB  /* Do not change */
+#define  MISO              4      /* Do not change */
 
-#define  MOSI_DDR          DDRB   
-#define  MOSI_PORT         PORTB  
-#define  MOSI              3      
+#define  MOSI_DDR          DDRB   /* Do not change */
+#define  MOSI_PORT         PORTB  /* Do not change */
+#define  MOSI              3      /* Do not change */
 
-#define  MCU_SS_DDR        DDRB   
-#define  MCU_SS_PORT       PORTB  
-#define  MCU_SS            2      
+#define  MCU_SS_DDR        DDRB   /* Do not change */
+#define  MCU_SS_PORT       PORTB  /* Do not change */
+#define  MCU_SS            2      /* Do not change */
 
-#define  GENERAL_CALL      0x00
-#define  ACK_WAIT_MS       10  
-#define  READ              0x01
-#define  WRITE             0x00
-#define  DOWN              0x02 
-#define  RX                0x01
-#define  TX                0x00
-#define  NA                0x00
-#define  CRC_LSBYTE_POS    31
-#define  CRC_MSBYTE_POS    30
-#define  LEN_BYTE_POS      29
-#define  RX_ADDR_BYTE_POS  28
-#define  OWN_ADDR_BYTE_POS 27
+#define  GENERAL_CALL      0x00   /* Do not change */
+#define  ACK_WAIT_MS       10     /* Do not change */
+#define  READ              0x01   /* Do not change */
+#define  WRITE             0x00   /* Do not change */
+#define  DOWN              0x02   /* Do not change */
+#define  RX                0x01   /* Do not change */
+#define  TX                0x00   /* Do not change */
+#define  NA                0x00   /* Do not change */
+#define  CRC_LSBYTE_POS    31     /* Do not change */
+#define  CRC_MSBYTE_POS    30     /* Do not change */
+#define  LEN_BYTE_POS      29     /* Do not change */
+#define  RX_ADDR_BYTE_POS  28     /* Do not change */
+#define  OWN_ADDR_BYTE_POS 27     /* Do not change */
 
 #define  CSN_LOW()         CSN_PORT&=~(1<<CSN)
 #define  CSN_HIGH()        CSN_PORT|=(1<<CSN)
@@ -223,13 +235,13 @@ return sts;
 }
 
 
-uint8_t RF_RX_TIMEOUT(uint8_t *buf, uint8_t *len, uint16_t timeout, uint8_t flash){
+uint8_t RF_RX_TIMEOUT(uint8_t *buf, uint8_t *len, uint16_t timeout, uint8_t flush){
 RF_PWR(RX);
 uint8_t dummy[2];
-if(flash){RF_RW_REG(0xE2,WRITE,dummy,0);}
-uint16_t ticks=0,sts=0;
+if(flush){RF_RW_REG(0xE2,WRITE,dummy,0);}
+uint16_t ticks=0,sts=0,tmout=timeout*10;
 
-while(ticks<timeout){
+while(ticks<tmout){
  sts=0; uint8_t temp[2]; uint16_t crc=0;
  RF_RW_REG(0x07,READ,temp,1);
  uint8_t data_pipe=(temp[0] & 0x0E)>>1;
@@ -259,7 +271,7 @@ if(rf.tpid>7){rf.tpid=0;}
 uint8_t sts=0,rty=0,temp_len=0,temp_pid=(rf.tpid<<5),rbuf[32];
 while(rty<retry){
   RF_TX(tbuf,tlen|temp_pid,rx_addr);
-  if(RF_RX_TIMEOUT(rbuf,&temp_len,(ACK_WAIT_MS*10),0)){
+  if(RF_RX_TIMEOUT(rbuf,&temp_len,ACK_WAIT_MS,0)){
     if((rbuf[RX_ADDR_BYTE_POS]==OWN_ADDR)&&(rf.tpid==(rbuf[LEN_BYTE_POS]>>5))){
 	   sts=1;
 	   break;
@@ -291,7 +303,7 @@ if(rf.tpid>7){rf.tpid=0;}
 uint8_t sts=0,rty=0,temp_len=0,temp_pid=(rf.tpid<<5);
 while(rty<retry){
   RF_TX(tbuf,tlen|temp_pid,rx_addr);
-  if(RF_RX_TIMEOUT(rbuf,&temp_len,(ACK_WAIT_MS*10),0)){
+  if(RF_RX_TIMEOUT(rbuf,&temp_len,ACK_WAIT_MS,0)){
     if((rbuf[RX_ADDR_BYTE_POS]==OWN_ADDR)&&(rf.tpid==(rbuf[LEN_BYTE_POS]>>5))){
 	   *rlen=temp_len;
 	   sts=1;
