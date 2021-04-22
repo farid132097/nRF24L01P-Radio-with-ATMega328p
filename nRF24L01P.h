0118f31990a50eb8577b9772a960e4c1a7dda2cc
farid132097 <82755186+farid132097@users.noreply.github.com>
2021-04-22 08:37:58 +0600

    Update nRF24L01P.h

--- nRF24L01P.h
+++ nRF24L01P.h
@@ -35,7 +35,7 @@ uint8_t      rx_buf               uint8_t type array (must be 32 bytes long)
 uint8_t      set_tx_data_len      single byte (max value 26)
 *uint8_t     get_rx_data_len      single byte (use &variable)
 uint8_t      set_rx_addr          device address which will receive data; single byte (max value 255)
-uint16_t     rx_timeout           set timeout in ms (max value 6500)
+uint16_t     rx_timeout           set timeout in 0.1ms (max value 65535)
 uint16_t     rx_buf_flush         boolean input (to flush every time before checking new data set it 1)
 uint8_t      tx_retry             max retries when no acknowledgement received (max value 255)
 */
